9db4b49b6c82de5e18db54276dfc9df2d9943e39
Edwin Simpson <edwin@klaybot.com>
2024-11-26 11:56:44 -0600

    components and image picker practice

--- fcc/WeatherApp/app/(tabs)/index.tsx
+++ fcc/WeatherApp/app/(tabs)/index.tsx
@@ -1,27 +1,55 @@
-import { View, StyleSheet, Text } from 'react-native';
-import CurrentWeather from '../../src/screens/CurrentWeather'
-import UpcommingWeather from '../../src/screens/UpcommingWeather'
-import City from '../../src/screens/City'
+import { View, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
+import { useState } from 'react';
+
+
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceholderImage = require('@/assets/images/city.jpg');
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      {/* <UpcommingWeather /> */}
-      {/* <City/> */}
-      {/* <CurrentWeather/> */}
-      <Text>Hola!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff'
-  }
-})
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+});
